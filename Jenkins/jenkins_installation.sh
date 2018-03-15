dd0910b66633e7a887256285240ac23062895a5e
AliaksandrTaran <32549052+AliaksandrTaran@users.noreply.github.com>
2018-03-15 22:44:05 +0200

    Update jenkins_installation.sh

--- Jenkins/jenkins_installation.sh
+++ Jenkins/jenkins_installation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-install java-1.8.0-openjdk-devel.x86_64
+yum -y install java-1.8.0-openjdk-devel.x86_64
 wget -O /etc/yum.repos.d/jenkins.repo http://jenkins-ci.org/redhat/jenkins.repo
 rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
 yum -y install jenkins
